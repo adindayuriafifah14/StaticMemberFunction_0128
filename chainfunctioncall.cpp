f757168125a06afb7d9caedc287f948af81800cc
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-05-05 20:20:41 +0700

    menambahkan class Buku dengan cara metode chainingfunctioncall

---
+++ chainfunctioncall.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Buku {
+    string judul;
+
+    public:
+        Buku &setJudul(string judul) {
+            this->judul = judul;
+            return *this; // chain function
+        }
+
+        string getJudul() {
+            return this->judul;
+        }
+};
+
+int main() {
+    Buku bukunya;
+    //bukunya.setJudul("matematika");
+    //cout << bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul() << endl; // chain function call
+    return 0;
+}
