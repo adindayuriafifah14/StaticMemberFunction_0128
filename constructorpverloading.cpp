5d647eee82ea08f26cfb9f9ec6b4719ef9ef7bf0
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-05-05 20:13:37 +0700

    menambahkan class Mahasiswa lengkap dengan constructor overloading

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -13,3 +13,36 @@ class Mahasiswa {
         Mahasiswa(int nim, string nama);
         void cetak();
 };
+
+Mahasiswa::Mahasiswa() {}
+
+Mahasiswa::Mahasiswa(int inim) {
+    nim = inim;
+}
+
+
+Mahasiswa::Mahasiswa(string inama) {
+    nama = inama;
+}
+
+Mahasiswa::Mahasiswa(int inim, string inama) {
+    nim = inim;
+    nama = inama;
+}
+
+void Mahasiswa::cetak() {
+    cout << "NIM =  " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
+
+int main() {
+    Mahasiswa mhs1(102030);
+    Mahasiswa mhs2("Andi");
+    Mahasiswa mhs3(010203, "Santi");
+
+    cout << "Mahasiswa 1 : " ; mhs1.cetak();
+    cout << "Mahasiswa 2 : " ; mhs2.cetak();
+    cout << "Mahasiswa 3 : ";  mhs3.cetak();
+    
+    return 0;
+}
