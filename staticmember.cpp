760c707d10a18d7084e9377096f820a8a1881f38
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-05-05 18:45:07 +0700

    menambahkan panggilan prosedur untuk printAll

--- staticmember.cpp
+++ staticmember.cpp
@@ -40,7 +40,14 @@ int main (){
     mahasiswa mhs3("Andi Kurniawan");
     mahasiswa mhs4("Joko Purbo");
 
-    
+    //memanggil prosedur printAll()
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    return 0;
+}
 
 
 
