38ef28eee06c220f73a654454ef9dc1a3ea2791e
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-05-05 19:11:45 +0700

    menambahkan tampilan untuk memanggil constractor dengan parameter

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -6,3 +6,10 @@ class mahasiswa {
 public:
     mahasiswa(int nim, string nama); //constructor dengan parameter
 };
+
+//definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {
+    cout << "Constractor dengan Parameter Terpanggil" << endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nma : " << nama << endl;
+}
